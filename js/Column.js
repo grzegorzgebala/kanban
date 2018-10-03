e0d32d7fb167ca98f5bf0dd4b27eed6606473e86
Grzegorz Gebala <grzegorz.gebala@yahoo.com>
2018-10-03 23:24:43 +0200

    final commit

--- js/Column.js
+++ js/Column.js
@@ -1,9 +1,11 @@
-function Column(name) {
+function Column(id, name) {
 	var self = this;
 	
-	this.id = randomString();
-	this.name = name;
+	this.id = id;
+    this.name = name || 'No name given';
 	this.element = createColumn();
+
+
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
@@ -19,8 +21,19 @@ function Column(name) {
 		});
 		
 		columnAddCard.click(function(event) {
+			var cardName = prompt("Enter the name of the card");
 			event.preventDefault();
-			self.createCard(new Card(prompt("Wpisz nazwę karty")));
+			$.ajax({
+		        url: baseUrl + '/card',
+		        method: 'POST',
+		        data: {
+		              //body query
+		        },
+		        success: function() {
+		            var card = new Card(response.id, cardName);
+        			self.createCard(card);
+		        }
+		    });
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
@@ -36,6 +49,13 @@ Column.prototype = {
 	  this.element.children('ul').append(card.element);
 	},
 	deleteColumn: function() {
-	  this.element.remove();
-	}
+    var self = this;
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'DELETE',
+      success: function(response){
+        self.element.remove();
+      }
+    });
+ }
 };
